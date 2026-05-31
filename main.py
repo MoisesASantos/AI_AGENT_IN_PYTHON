91ad50e7934fcbb7c796b4f1adf7c82f760cfef0
TsukuyomiT23 <moisessantosa@gmail.com>
2026-05-31 19:28:26 +0100

    add the feature to get a prompt for command line, use argparse for this

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
 
 def main():
+	parser = argparse.ArgumentParser(description="ChatBot")
+	parser.add_argument("user_prompt", type=str, help="user prompt")
+	arg = parser.parse_args()
 	load_dotenv()
 	api_key = os.environ.get("GEMINI_API_KEY")
 	if api_key is None:
@@ -10,7 +15,7 @@ def main():
 	client = genai.Client(api_key=api_key)
 	response = client.models.generate_content(
 		model="gemini-2.5-flash",
-		contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+		contents=arg.user_prompt
 	)
 	if response.usage_metadata is None:
 		raise RunTimeError("API request fail, try again") 
