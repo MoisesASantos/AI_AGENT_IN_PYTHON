a40dc1cce54a64893480685a10e98808e46c5d12
TsukuyomiT23 <moisessantosa@gmail.com>
2026-05-31 19:12:16 +0100

    add the message for tokens consume in prompt and response

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def main():
 	load_dotenv()
 	api_key = os.environ.get("GEMINI_API_KEY")
 	if api_key is None:
-		raise runTimeError("Not found a API KEY for the model")
+		raise RunTimeError("Not found a API KEY for the model")
 	client = genai.Client(api_key=api_key)
 	response = client.models.generate_content(
 		model="gemini-2.5-flash",
 		contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 	)
+	if response.usage_metadata is None:
+		raise RunTimeError("API request fail, try again") 
+	print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+	print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 	print(response.text)
 
 
