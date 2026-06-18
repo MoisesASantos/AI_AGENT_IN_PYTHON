cf133881495dd0b60c731b67052b93c172120b25
MoisesASantos <moisessantosa23@gmail.com>
2026-06-18 14:38:07 +0100

    calling functions added

--- call_function.py
+++ call_function.py
@@ -1,9 +1,24 @@
 from google.genai import types
+from collections.abc import Callable
+from functions.get_files_info import (
+    get_files_info,
+    schema_get_files_info,
+)
 
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_file_content import (
+    get_file_content,
+    schema_get_file_content,
+)
+
+from functions.run_python_file import (
+    run_python_file,
+    schema_run_python_file,
+)
+
+from functions.write_file import (
+    write_file,
+    schema_write_file,
+)
 
 
 available_functions = types.Tool(
@@ -14,3 +29,60 @@ available_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+function_map: dict[str, Callable[..., str]] = {
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+	"run_python_file": run_python_file,
+	"write_file": write_file
+}
+
+def call_function(
+    function_call: types.FunctionCall,
+    verbose: bool = False,
+) -> types.Content:
+    #check if verbose flag are setted
+    if verbose:
+        print(
+            f"Calling function: "
+            f"{function_call.name}({function_call.args})"
+        )
+    else:
+        print(
+            f" - Calling function: {function_call.name}"
+        )
+	#get the name function from the args of callable function
+    function_name = function_call.name or ""
+
+	#check if the name exist in our dict of functions
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={
+                        "error": (
+                            f"Unknown function: {function_name}"
+                        )
+                    },
+                )
+            ],
+        )
+
+	#get the args of the function we are callin
+    args = dict(function_call.args) if function_call.args else {}
+    args["working_directory"] = "./calculator"
+
+	#execute and catch the result of function calling 
+    function_result = function_map[function_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
